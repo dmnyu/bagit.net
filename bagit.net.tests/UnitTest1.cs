c5c2c51438475f4c4d46ae0d698d7d71ebba497c
Don Mennerich <don.mennerich@nyu.edu>
2025-11-12 16:08:44 -0500

    setting up unit testing.

--- bagit.net.tests/UnitTest1.cs
+++ bagit.net.tests/UnitTest1.cs
@@ -1,10 +1,36 @@
-﻿namespace bagit.net.tests;
-
-public class UnitTest1
+﻿namespace bagit.net.tests
 {
-    [Fact]
-    public void Test1()
+    public class BagitTests
     {
+        [Fact]
+        public void IsEquals_ReturnsTrue_WhenNumbersAreEqual()
+        {
+            // Arrange
+            var bagit = new Bagit();
+            int a = 5;
+            int b = 5;
 
+            // Act
+            bool result = bagit.IsEquals(a, b);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsEquals_ReturnsFalse_WhenNumbersAreNotEqual()
+        {
+            // Arrange
+            var bagit = new Bagit();
+            int a = 5;
+            int b = 10;
+
+            // Act
+            bool result = bagit.IsEquals(a, b);
+
+            // Assert
+            Assert.False(result);
+        }
     }
 }
+
--- bagit.net/Program.cs
+++ bagit.net/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace bagit.net;
+
+public class Bagit
+{
+    public bool IsEquals(int i, int j)
+    {
+        return i == j;
+    }
+}
