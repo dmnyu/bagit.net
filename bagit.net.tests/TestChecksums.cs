edc8595f6ded9acbc6c9e3df08172619c224cde3
Don Mennerich <don.mennerich@nyu.edu>
2025-11-14 09:53:51 -0500

    Adding test for checksum calculation on files

--- bagit.net.tests/TestChecksums.cs
+++ bagit.net.tests/TestChecksums.cs
@@ -25,8 +25,34 @@ namespace bagit.net.tests
                 var emptyFile = Path.Combine(tmpDir, "empty.txt");
                 File.WriteAllBytes(emptyFile, Array.Empty<byte>());
                 foreach (var kv in expectedChecksums) {
-                    Assert.True(Checksum.CompareChecksum(emptyFile, kv.Value, kv.Key));
+                    Assert.True(Checksum.CompareChecksum(emptyFile, kv.Value, kv.Key), $"{kv.Key} failed");
                 }
+            } finally
+            {
+                Directory.Delete(tmpDir, true);
+            }
+        }
+
+        [Fact]
+        public void Test_Calulate_Checksums_For_File()
+        {
+            var tmpDir = string.Empty;
+            try
+            {
+                tmpDir = TestHelpers.PrepareTempTestData();
+                var file = Path.Combine(tmpDir, "Dir", "hello.txt");
+                var expectedChecksums = new Dictionary<ChecksumAlgorithm, string> {
+                    { ChecksumAlgorithm.MD5, "d41d8cd98f00b204e9800998ecf8427e" },
+                    { ChecksumAlgorithm.SHA1, "da39a3ee5e6b4b0d3255bfef95601890afd80709" },
+                    { ChecksumAlgorithm.SHA256, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" },
+                    { ChecksumAlgorithm.SHA384, "38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b" },
+                    { ChecksumAlgorithm.SHA512, "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e" }
+                };
+
+                foreach (var kv in expectedChecksums) {
+                    Assert.True(Checksum.CalculateChecksum(file, kv.Key) == kv.Value,$"{kv.Key} failed");
+                }
+
             } finally
             {
                 Directory.Delete(tmpDir, true);
