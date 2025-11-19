efa85568898f9b3e26f113caa5082e047c42f93c
Donald Mennerich <don.mennerich@nyu.edu>
2025-11-19 11:36:13 -0500

    validating bag-info.txt

---
+++ bagit.net.tests/TestValidateBag.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestPlatform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bagit.net.tests
+{
+    public class TestValidateBag : IDisposable
+    {
+        private readonly string _tmpDir;
+        private readonly string _bagDir;
+        private readonly Validator _validator;
+        
+        public TestValidateBag()
+        {
+            _tmpDir = TestHelpers.PrepareTempTestData();
+            _bagDir = Path.Combine(_tmpDir, "bagged-dir");
+            _validator = new Validator();
+        }
+
+        public void Dispose()
+        {
+            if(Directory.Exists(_tmpDir))
+                Directory.Delete(_tmpDir, true);
+        }
+
+        [Fact]
+        public void Test_Bag_Exists() 
+        {
+            Assert.True(Directory.Exists(_bagDir));
+        }
+
+        [Fact]
+        public void Test_Has_Valid_BagitTXT()
+        {
+            var ex = Record.Exception(() => _validator.Has_Valid_BagitTXT(_bagDir));
+            Assert.Null(ex);
+        }
+
+
+    }
+}
---
+++ bagit.net/Validator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bagit.net
+{
+    public class Validator
+    {
+
+        internal void Has_Valid_BagitTXT(string path)
+        {
+            var bagitPath = Path.Combine(path, "bagit.txt");
+            if (!File.Exists(bagitPath))
+                throw new FileNotFoundException("bagit.txt is missing from the bag root.", bagitPath);
+
+            var tags = TagFile.GetTagFileAsDict(bagitPath);
+
+            if (!tags.TryGetValue("BagIt-Version", out var version))
+                throw new FormatException("BagIt-Version key is missing in bagit.txt.");
+
+            if (!System.Text.RegularExpressions.Regex.IsMatch(version, @"^\d+\.\d+$"))
+                throw new FormatException($"Invalid BagIt-Version format: {version}");
+
+            if (!tags.TryGetValue("Tag-File-Character-Encoding", out var encoding))
+                throw new FormatException("Tag-File-Character-Encoding key is missing in bagit.txt.");
+
+            if (!string.Equals(encoding, "UTF-8", StringComparison.OrdinalIgnoreCase))
+                throw new FormatException($"Unsupported Tag-File-Character-Encoding: {encoding}");
+        }
+    }
+}
